0b5313bf39fbca0798fa2e07895dbabc435697ab
Nekomochi <53544375+nekom0chi@users.noreply.github.com>
2026-07-08 17:05:22 +0900

    Add executable icon

--- native/build_mingw.bat
+++ native/build_mingw.bat
@@ -10,9 +10,21 @@ if errorlevel 1 (
   set "GPP=%LOCALAPPDATA%\Microsoft\WinGet\Packages\BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\bin\g++.exe"
 )
 
+set "WINDRES=windres"
+where windres >nul 2>nul
+if errorlevel 1 (
+  set "WINDRES=%LOCALAPPDATA%\Microsoft\WinGet\Packages\BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\bin\windres.exe"
+)
+
+"%WINDRES%" app_icon.rc -O coff -o app_icon.o
+if errorlevel 1 (
+  echo Resource build failed.
+  exit /b 1
+)
+
 "%GPP%" -std=c++17 -O2 -municode -mwindows -static -static-libgcc -static-libstdc++ inzone_buds_battery.cpp ^
   -o "..\dist-native\INZONE Buds Battery.exe" ^
-  -luser32 -lshell32 -lgdi32
+  app_icon.o -luser32 -lshell32 -lgdi32
 
 if errorlevel 1 (
   echo Build failed.
--- native/build_msvc.bat
+++ native/build_msvc.bat
@@ -4,9 +4,15 @@ cd /d "%~dp0"
 
 if not exist "..\dist-native" mkdir "..\dist-native"
 
+rc /nologo /fo app_icon.res app_icon.rc
+if errorlevel 1 (
+  echo Resource build failed.
+  exit /b 1
+)
+
 cl /nologo /std:c++17 /EHsc /O2 /utf-8 /DUNICODE /D_UNICODE inzone_buds_battery.cpp ^
   /Fe:"..\dist-native\INZONE Buds Battery.exe" ^
-  /link /SUBSYSTEM:WINDOWS user32.lib shell32.lib gdi32.lib
+  /link /SUBSYSTEM:WINDOWS app_icon.res user32.lib shell32.lib gdi32.lib
 
 if errorlevel 1 (
   echo Build failed.
